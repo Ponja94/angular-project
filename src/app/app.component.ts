2b4782cc7b4b2d90612217a84176dfa44e786ae5
Ponja94 <henrique.danielb@gmail.com>
2022-04-26 16:13:03 -0300

    2nd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = 'Joaquin';
+
+  userData = {
+    email: 'joaquin@gmail.com',
+    role: 'Admin',
+  }
+
   title = 'curso-angular';
 }
